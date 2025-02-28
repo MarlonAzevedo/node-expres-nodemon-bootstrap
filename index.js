3730b4fbb7da16f4bd80977a6f03db2095ce900f
Marlon Azevedo <foxwnobr@gmail.com>
2025-02-28 11:29:26 -0300

    atualização

--- index.js
+++ index.js
@@ -5,22 +5,8 @@ const port = 3000;
 app.set('view engine','ejs');
 app.use(express.static('public'));
 
-app.get("/:nome/:tel",(req,res)=>{
-    var nome = req.params.nome;
-    var tel = req.params.tel;
-    var msg = false;
-    var produtos = [
-        {nome:"koka kola",preco:10},
-        {nome:"pepcy kola",preco:8},
-        {nome:"dolinho", preco:5}
-    ];
-
-    res.render("index",{
-        nome:nome,
-        tel:tel,
-        msg:msg,
-        produtos:produtos
-    });
+app.get("/",(req,res)=>{
+   
 });
 
 app.get("/sobre",(rec,res)=>{
